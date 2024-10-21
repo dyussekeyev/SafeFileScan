0317229e0dedb986ea63e43b22d298b854ce14f0
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-22 02:05:33 +0500

    Update index.php

--- superadmin/index.php
+++ superadmin/index.php
@@ -3,8 +3,19 @@ session_start();
 include '../includes/db.php';
 include '../includes/functions.php';
 checkSuperAdminAuth();
-
-echo '<h1>SuperAdmin Dashboard</h1>';
-echo '<a href="manage_admins.php">Manage Admins</a><br>';
-echo '<a href="view_logs.php">View Logs</a><br>';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SuperAdmin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+
+<h1>SuperAdmin Dashboard</h1>
+<a href="manage_admins.php">Manage Admins</a><br>
+<a href="view_logs.php">View Logs</a><br>
+
+</body>
+</html>
