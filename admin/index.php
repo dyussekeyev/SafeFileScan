d3ae9e8736045c66cb6c4cf8c2ba53631257c9e2
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-21 21:10:46 +0500

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include '../includes/db.php';
+include '../includes/functions.php';
+checkAdminAuth();
+
+echo '<h1>Admin Dashboard</h1>';
+echo '<a href="upload.php">Upload File</a><br>';
+echo '<a href="delete_scan.php">Delete Scan</a><br>';
+echo '<a href="delete_file.php">Delete File</a><br>';
+?>
