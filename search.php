8a918b40875c93d44281904f0f9a5a9dff830975
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-21 21:48:50 +0500

    Update search.php

--- search.php
+++ search.php
@@ -8,12 +8,39 @@ if (isset($_GET['hash'])) {
     $fileInfo = searchFileByHash($hash);
     
     if ($fileInfo) {
-        // Display file info and scan results
-        echo 'File Info:';
-        echo '<pre>' . print_r($fileInfo, true) . '</pre>';
-        echo 'Scan Results:';
+        // Display file info in a table
+        echo '<h2>File Info</h2>';
+        echo '<table border="1">';
+        echo '<tr><th>Filename</th><th>MD5</th><th>SHA1</th><th>SHA256</th><th>Imphash</th><th>Size</th><th>First Upload Date</th><th>Last Analysis Date</th></tr>';
+        echo '<tr>';
+        echo '<td>' . htmlspecialchars($fileInfo['filename']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['md5']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['sha1']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['sha256']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['imphash']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['size']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['first_upload_date']) . '</td>';
+        echo '<td>' . htmlspecialchars($fileInfo['last_analysis_date']) . '</td>';
+        echo '</tr>';
+        echo '</table>';
+
+        // Display scan results in multiple tables
+        echo '<h2>Scan Results</h2>';
         $scanResults = getScanResults($fileInfo['md5']);
-        echo '<pre>' . print_r($scanResults, true) . '</pre>';
+        if ($scanResults) {
+            foreach ($scanResults as $result) {
+                echo '<table border="1">';
+                echo '<tr><th>Antivirus Name</th><th>Scan Date</th><th>Result</th></tr>';
+                echo '<tr>';
+                echo '<td>' . htmlspecialchars($result['antivirus_name']) . '</td>';
+                echo '<td>' . htmlspecialchars($result['scan_date']) . '</td>';
+                echo '<td>' . htmlspecialchars($result['result']) . '</td>';
+                echo '</tr>';
+                echo '</table><br>';
+            }
+        } else {
+            echo 'No scan results found for this file.';
+        }
     } else {
         echo 'No file found with the given hash.';
     }
