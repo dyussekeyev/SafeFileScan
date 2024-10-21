f5e3fa7b96e6aa6fbeb6a1be055c07ce15ab62a2
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-22 02:05:18 +0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,8 +3,19 @@ session_start();
 include '../includes/db.php';
 include '../includes/functions.php';
 checkAdminAuth();
-
-echo '<h1>Admin Dashboard</h1>';
-echo '<a href="delete_scan.php">Delete Scan</a><br>';
-echo '<a href="delete_file.php">Delete File</a><br>';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+
+<h1>Admin Dashboard</h1>
+<a href="delete_scan.php">Delete Scan</a><br>
+<a href="delete_file.php">Delete File</a><br>
+
+</body>
+</html>
