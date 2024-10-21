08c084fd98dc28b387334a339ba696ae114fba09
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-21 21:46:58 +0500

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,8 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS virustotal_clone;
+USE virustotal_clone;
+
+-- Create tables
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
