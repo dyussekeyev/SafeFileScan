f8f66f85165b6c84ec98b08409d0bbc1dfeae2f2
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-22 02:25:09 +0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -33,7 +33,7 @@ if (isset($_GET['logout'])) {
 ?>
 
 <div class="navbar">
-    <a href="index.php" style="float: left;">Home</a>
+    <a href="/index.php" style="float: left;">Home</a>
     
     <!-- Centered search form -->
     <form method="get" action="search.php" style="text-align: center; margin: 0 auto;">
