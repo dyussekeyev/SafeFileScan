67942be991d21e2b861e883609dcad8b7088d335
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-22 00:58:32 +0500

    Update search.php

--- search.php
+++ search.php
@@ -7,25 +7,42 @@
             overflow: hidden;
             background-color: #333;
         }
-        .navbar a {
+        .navbar a, .navbar input[type=text], .navbar input[type=password], .navbar input[type=submit] {
             float: left;
             display: block;
             color: #f2f2f2;
             text-align: center;
             padding: 14px 16px;
             text-decoration: none;
+            border: none;
+            background: none;
         }
-        .navbar a:hover {
+        .navbar input[type=text], .navbar input[type=password] {
+            background-color: #ddd;
+            color: black;
+            padding: 6px;
+            margin-top: 8px;
+            margin-right: 2px;
+            margin-left: 2px;
+            border-radius: 4px;
+        }
+        .navbar input[type=submit] {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 16px;
+            margin-top: 8px;
+            margin-right: 2px;
+            margin-left: 2px;
+            border-radius: 4px;
+        }
+        .navbar a:hover, .navbar input[type=submit]:hover {
             background-color: #ddd;
             color: black;
         }
     </style>
 </head>
 <body>
-    <div class="navbar">
-        <a href="index.php">Home</a>
-        <a href="login.php">Log in</a>
-    </div>
+    <?php include 'navbar.php'; ?>
 
 <?php
 include 'includes/db.php';
@@ -82,4 +99,5 @@ if (isset($_GET['hash'])) {
 }
 ?>
 
-</body></html>
+</body>
+</html>
