ae469e3d8c28253f0a9e64cd87ca419aa217ee99
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-23 11:32:43 +0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,7 +1,5 @@
 <div class="navbar">
     <a href="./index.php" style="float: left;">Home</a>
-    
-    <!-- Centered search form -->
     <form method="get" action="search.php" style="text-align: center; margin: 0 auto;">
         <input type="text" name="hash" placeholder="Search by hash" required>
         <input type="submit" value="Search">
