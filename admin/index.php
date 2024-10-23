4c6e268b76d1cbccd2f155523592f177a02df2e5
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-23 11:00:02 +0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $scanId = $_POST['scan_id'];
         deleteScanResult($scanId);
         echo 'Scan result deleted successfully.';
+    } else if (isset($_POST['create_admin'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $role = $_POST['role'];
+        createAdmin($username, $password, $role);
+        echo 'Admin created successfully.';
+    } else if (isset($_POST['change_password'])) {
+        $username = $_POST['username'];
+        $newPassword = $_POST['new_password'];
+        changeAdminPassword($username, $newPassword);
+        echo 'Password changed successfully.';
     }
 }
 ?>
@@ -43,6 +54,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     Enter Scan ID to delete:
     <input type="text" name="scan_id">
     <input type="submit" value="Delete Scan">
+</form>
+
+<h2>Create Admin</h2>
+<form action="index.php" method="post">
+    <input type="hidden" name="create_admin" value="1">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password" name="password"><br>
+    Role: 
+    <select name="role">
+        <option value="admin">Admin</option>
+        <option value="superadmin">SuperAdmin</option>
+    </select><br>
+    <input type="submit" value="Create Admin">
+</form>
+
+<h2>Change Admin Password</h2>
+<form action="index.php" method="post">
+    <input type="hidden" name="change_password" value="1">
+    Username: <input type="text" name="username"><br>
+    New Password: <input type="password" name="new_password"><br>
+    <input type="submit" value="Change Password">
 </form>
 
 </body>
