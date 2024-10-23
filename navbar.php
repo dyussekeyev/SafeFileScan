479f4dd33ffe6a9a273a3598d814c7d07165d188
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2024-10-23 11:33:42 +0500

    Delete navbar.php

--- navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="navbar">
-    <a href="./index.php" style="float: left;">Home</a>
-    <form method="get" action="search.php" style="text-align: center; margin: 0 auto;">
-        <input type="text" name="hash" placeholder="Search by hash" required>
-        <input type="submit" value="Search">
-    </form>
-</div>
